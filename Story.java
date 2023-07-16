5560c20c40b798655e0c38e84835ad698e64e5ae
Y-oshi06 <138310640+Y-oshi06@users.noreply.github.com>
2023-07-16 16:33:37 +0900

    Update Story.java

--- Story.java
+++ Story.java
@@ -4,34 +4,24 @@ public class Story {
 	public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-		hero h = new hero();//勇者
-		System.out.println("勇者の名前を入力してください");
-		h.name = scan.nextLine();
-		System.out.println("体力を入力してください");
-		h.hp = scan.nextInt();
-		System.out.println("攻撃力を入力してください");
-		h.at = scan.nextInt();
-		System.out.println("守備力を入力してください");
+		hero h = new hero("ルフィ",19);//勇者
+		System.out.println("ヒーローの守備力を入力してください");
 		h.df = scan.nextInt();
 
 		System.out.println("名前:" + h.name);
 		System.out.println("体力:" + h.hp);
 		System.out.println("攻撃力:" + h.at);
 		System.out.println("守備力:" + h.df);
+		System.out.println(" ");
 		
 		//doragon
 		System.out.println(" ");
 		Scanner scan2 = new Scanner(System.in);
-		doragon g = new doragon();//
+		doragon g = new doragon("カイドウ");//
 		
-		System.out.println("ドラゴンの名前を入力してください");
-		g.name = scan2.nextLine();
-		
-		System.out.println("体力を入力してください");
-		g.hp = scan2.nextInt();
-		System.out.println("攻撃力を入力してください");
+		System.out.println("ドラゴンの攻撃力を入力してください");
 		g.at = scan2.nextInt();
-		System.out.println("守備力を入力してください");
+		System.out.println("ドラゴンの守備力を入力してください");
 		g.df = scan2.nextInt();
 		
 	
@@ -39,18 +29,15 @@ public class Story {
 		System.out.println("体力:" + g.hp);
 		System.out.println("攻撃力:" + g.at);
 		System.out.println("守備力:" + g.df);
+		System.out.println(" ");
 		
 		//魔法使い
 		System.out.println(" ");
 		Scanner scan3 = new Scanner(System.in);
-		Magician m = new Magician();//勇者
-		System.out.println("魔法使いの名前を入力してください");
-		m.name = scan3.nextLine();
-		System.out.println("体力を入力してください");
-		m.hp = scan3.nextInt();
-		System.out.println("攻撃力を入力してください");
+		Magician m = new Magician("ゾロ");//
+		System.out.println("魔法使いの攻撃力を入力してください");
 		m.at = scan3.nextInt();
-		System.out.println("守備力を入力してください");
+		System.out.println("魔法使いの守備力を入力してください");
 		m.df = scan3.nextInt();
 		m.MP = 10;
 		m.recovery = 10;
